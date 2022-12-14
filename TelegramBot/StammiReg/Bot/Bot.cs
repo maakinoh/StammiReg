b42187272ab95c318175e9e0cd634912c4b24d66
Maakinoh <maakinoh@furrytrash.de>
2022-12-14 09:47:29 +0100

    init

---
+++ TelegramBot/StammiReg/Bot/Bot.cs
@@ -0,0 +1,39 @@
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TelegramBot.StammiReg.Bot;
+
+public class Bot
+{
+    private const string TOKEN_ENV_NAME = "STAMMIREG-TOKEN";
+    private readonly TelegramBotClient bot;
+
+
+    public Bot()
+    {
+        bot = new TelegramBotClient(Environment.GetEnvironmentVariable(TOKEN_ENV_NAME) ??
+                                    throw new InvalidOperationException("Telegram bot token is missing"));
+
+        bot.StartReceiving(
+            HandleUpdateAsync,
+            HandlePollingErrorAsync
+        );
+    }
+
+
+    private async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update,
+        CancellationToken cancellationToken)
+    {
+        if (update.Message is not { } message)
+            return;
+        // Only process text messages
+        if (message.Text is not { } messageText)
+            return;
+    }
+
+    private Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception,
+        CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
---
+++ TelegramBot/StammiReg/Bot/Commands.cs
@@ -0,0 +1,11 @@
+namespace TelegramBot.StammiReg.Bot;
+
+public enum Commands
+{
+    Idle,
+    StartReg,
+
+    // Enters whenever the user want's to attend just the fursuit walk, the furmeet or both
+    EnterScope,
+    Confirm
+}
